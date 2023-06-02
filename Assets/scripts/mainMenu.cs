2afb93bd73ade689b3928e95090b93c50b13e028
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-06-02 20:13:56 +0530

    home screen added

---
+++ Assets/scripts/mainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainMenu : MonoBehaviour
+{
+
+   // load scene
+   public void Play()
+   {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+}
